940a89d663c098252c3911213d854271adfb4111
Victor <aue228lublupacanov@gmail.com>
2025-04-01 16:27:16 +0200

    created loading view for login

--- src/app/login/page.js
+++ src/app/login/page.js
@@ -2,87 +2,91 @@
 
 "use client";
 
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import { useRouter } from "next/navigation";
 import config from "../../config";
 import { setCookie } from "nookies";
+
 
 export default function LoginPage() {
     const [username, setUsername] = useState("");
     const [password, setPassword] = useState("");
     const [error, setError] = useState("");
+    const [loading, setLoading] = useState(false)
+    const [currentSlide, setCurrentSlide] = useState(0)
     const router = useRouter();
 
+    const images = [
+        "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQMopW-mYcbgg5N759gGD96shk3KtYC39jwKg&s",
+        "https://images.prismic.io/doge/c8f63200-9fd6-4fbb-ae72-4ddc8418375c_maltipu-15.jpg?auto=compress,format&rect=0,0,1080,1080&w=456&h=456",
+        "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRmi5RtC9Oa_qgjFP3ROldlwj4c9ohoEZTmUg&s"
+    ]
+
+    useEffect(() => {
+        if (loading) {
+            const interval = setInterval(() => {
+                setCurrentSlide((prev) => (prev + 1) % images.length)
+            }, 2000);
+            return () => clearInterval(interval)
+        }
+    }, [loading])
+
     const handleLogin = async (event) => {
-        event.preventDefault();
+        event.preventDefault()
+        setLoading(true)
+        setError("")
+        try {
+            const response = await fetch(`${config.API_BASE_URL}/auth/login/`, {
+                method: "POST",
+                headers: {
+                    "Content-Type": "application/json",
+                },
+                body: JSON.stringify({
+                    username: username,
+                    password: password,
+                }),
+            });
 
-        const response = await fetch(`${config.API_BASE_URL}/auth/login/`, {
-            method: "POST",
-            headers: {
-                "Content-Type": "application/json",
-            },
-            body: JSON.stringify({
-                username: username,
-                password: password,
-            }),
-        });
+            const data = await response.json();
 
-        const data = await response.json();
+            if (response.ok) {
+                console.log("Успешный вход:", data);
+                setCookie(null, "accessToken", data.token, {
+                    maxAge: 60 * 60 * 24,
+                    path: "/",
+                })
+                router.push("/storage");
+            } else {
+                throw new Error(data.detail || "Try again")
+            }
+        } catch (err) {
 
-        if (response.ok) {
-            console.log("Успешный вход:", data);
-            setCookie(null, "accessToken", data.token, {
-                maxAge: 60 * 60 * 24,
-                path: "/",
-            })
-            router.push("/storage"); // Перенаправление после входа
-        } else {
-            console.error("Ошибка входа:", data);
             setError("Ошибка входа: " + (data.detail || "Попробуйте снова"));
+        } finally {
+            setLoading(false)
         }
     };
 
 
     return (
-        // <div className="flex flex-col items-center justify-center min-h-screen bg-gray-100">
-        //     <div className="bg-white p-6 rounded-lg shadow-lg w-96">
-        //         <h2 className="text-2xl font-semibold text-center mb-4">Вход</h2>
-        //         {error && <p className="text-red-500">{error}</p>}
-        //         <input
-        //             type="text"
-        //             placeholder="Имя пользователя"
-        //             value={username}
-        //             onChange={(e) => setUsername(e.target.value)}
-        //             className="w-full p-2 border rounded mb-2"
-        //         />
-        //         <input
-        //             type="password"
-        //             placeholder="Пароль"
-        //             value={password}
-        //             onChange={(e) => setPassword(e.target.value)}
-        //             className="w-full p-2 border rounded mb-2"
-        //         />
-        //         <button
-        //             onClick={handleLogin}
-        //             className="w-full bg-blue-500 text-white py-2 rounded hover:bg-blue-600"
-        //         >
-        //             Войти
-        //         </button>
-        //     </div>
-
-
-
-        // </div>
         <div className="container">
-            <div className="form">
-                <div className="title">Welcome,<br /><span>sign in to continue</span></div>
-                <input className="input" name="email" placeholder="Username" type="email" value={username} onChange={(e) => setUsername(e.target.value)} />
-                <input className="input" name="password" placeholder="Password" type="password" value={password} onChange={(e) => setPassword(e.target.value)} />
+            {loading ? (
+                <div className="slideshow">
+                    <img src={images[currentSlide]} alt="Loading..." className="slide" />
+                    <p>Пожалуйста, подождите...(ради мальтипу)</p>
+                </div>
+            ) : (
+
+                <div className="form">
+                    <div className="title">Welcome,<br /><span>sign in to continue</span></div>
+                    <input className="input" name="email" placeholder="Username" type="email" value={username} onChange={(e) => setUsername(e.target.value)} />
+                    <input className="input" name="password" placeholder="Password" type="password" value={password} onChange={(e) => setPassword(e.target.value)} />
 
 
-                <button className="button-confirm" onClick={handleLogin}>Let`s go →</button>
-                {error && <p className="error-message">{error}</p>}
-            </div>
+                    <button className="button-confirm" onClick={handleLogin}>Let`s go →</button>
+                    {error && <p className="error-message">{error}</p>}
+                </div>
+            )}
         </div>
 
     );
