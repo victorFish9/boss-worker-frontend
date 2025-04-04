a6bf255e51243ac5e04f61a6e78c3d225b13b0ae
Victor <aue228lublupacanov@gmail.com>
2025-04-04 13:58:09 +0200

    edited design of navbar

--- src/config.js
+++ src/config.js
@@ -1,7 +1,7 @@
 const config = {
-    // API_BASE_URL: "http://localhost:3001", // Для локальной разработки
+    API_BASE_URL: "http://localhost:3001", // Для локальной разработки
     // API_BASE_URL: "https://boss-worker-platform.onrender.com", // Для продакшна (раскомментировать при деплое)
-    API_BASE_URL: "https://boss-worker-platformjs.onrender.com"
+    // API_BASE_URL: "https://boss-worker-platformjs.onrender.com"
 };
 
 export default config;
