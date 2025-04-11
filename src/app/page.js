c773cf7a5954b329c8895a63194980f6d7025d7a
Victor <aue228lublupacanov@gmail.com>
2025-04-11 10:25:31 +0200

    updated get apis

--- src/app/page.js
+++ src/app/page.js
@@ -22,14 +22,14 @@ export default function StoragePage() {
         const fetchFiles = async () => {
 
             try {
-                const response = await fetch(`${config.API_BASE_URL}/files/list/boss-worker-bucket`, {
+                const response = await fetch(`${config.API_BASE_URL}/files/google/list`, {
                     headers: {
                         Authorization: token,
                     }
                 });
 
                 const data = await response.json();
-                setFiles(data.files);
+                setFiles(data);
             } catch (err) {
                 setError(err.message);
             }
@@ -40,57 +40,35 @@ export default function StoragePage() {
 
     return (
         <div className="p-4 mt-6 border rounded-lg shadow bg-gray-50">
-
             {error && <p className="text-red-500">{error}</p>}
             <h1 className="text-lg font-semibold mb-3">Files available for download</h1>
 
             <div className="card_storage">
                 {files.length > 0 ? (
                     <ul className="card_storage_ul">
-                        {files
-                            .sort((a, b) => new Date(b.lastModified) - new Date(a.lastModified)) // Сортировка по дате
-                            .map((file, index) => {
-                                let parsedTags = [];
-                                try {
-                                    parsedTags = JSON.parse(file.tags);
-                                } catch (e) {
-                                    console.error("Ошибка парсинга tags:", e);
-                                }
-
-                                return (
-                                    <li key={index} className="card_storage_li list-item">
-                                        <div className="download_div">
-                                            <a
-                                                href={`${config.API_BASE_URL}/files/download/${file.bucket}/${file.name}`}
-                                                className="download-link"
-                                            >
-                                                Download
-                                            </a>
-                                            <h3 className="card__title font-medium">{file.name}</h3>
-                                        </div>
-                                        <div className="card__content text-sm text-gray-500">
-                                            <strong>Description:</strong>
-                                            {parsedTags.length > 0 ? (
-                                                <ul className="list-disc pl-5">
-                                                    {parsedTags.map((tag, tagIndex) => (
-                                                        <li key={tagIndex}>
-                                                            <span className="font-semibold">{tag.Key}:</span> {tag.Value}
-                                                        </li>
-                                                    ))}
-                                                </ul>
-                                            ) : (
-                                                <span>No tags</span>
-                                            )}
-                                        </div>
-                                    </li>
-                                );
-                            })}
+                        {files.map((file, index) => (
+                            <li key={index} className="card_storage_li list-item">
+                                <div className="download_div">
+                                    <a
+                                        href={config.API_BASE_URL + file.apiDownloadLink}
+                                        className="download-link"
+                                        target="_blank"
+                                        rel="noopener noreferrer"
+                                    >
+                                        Download
+                                    </a>
+                                    <h3 className="card__title font-medium">{file.name}</h3>
+                                </div>
+                                <div className="card__content text-sm text-gray-500">
+                                    <strong>Type:</strong> {file.type}
+                                </div>
+                            </li>
+                        ))}
                     </ul>
                 ) : (
                     <p>No files available.</p>
                 )}
             </div>
-
         </div>
     );
 
